3f6a7c99e6a4b1ba1c339b366cfcb393f06a58db
David García Díaz <davidgrcdiaz@gmail.com>
2024-03-21 19:34:33 +0100

    Add CORS configuration to Socket.IO server

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,11 @@ dotenv.config()
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  cors: {
+    origin: process.env.ALLOWED_HOSTS
+  }
+})
 
 const {WEBHOOK_SECRET} = process.env
 if (!WEBHOOK_SECRET) {
@@ -32,46 +36,3 @@ app.post('/webhook', (req, res) => {
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
-
-/*
-app.get('/', (req, res) => {
-  res.send(`<doctype HTML>
-  <html>
-    <head>
-      <title>My first express app</title>
-    </head>
-    <body>
-    <ul id="messages"></ul>
-    <form id="form" action="">
-      <input name="message" autocomplete="off" /><button>Send</button>
-    </form>
-    <script src="https://cdn.socket.io/4.7.5/socket.io.min.js"></script>
-    <script>
-    const form = document.querySelector('form')
-    const messages = document.getElementById('messages');
-    
-    const socket = io()
-
-    socket.on('chat message', (msg) => {
-      const item = document.createElement('li');
-      item.textContent = msg;
-      messages.appendChild(item);
-      window.scrollTo(0, document.body.scrollHeight);
-    });
-
-
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      const formdata = new FormData(e.target)
-      const message = formdata.get('message')
-      if (!message) return
-      socket.emit('chat message', message)
-      e.target.reset()
-    }
-    form.addEventListener('submit', handleSubmit)
-    </script>
-  </body>
-  </html>
-`);
-});
-*/
