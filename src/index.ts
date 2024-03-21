4ba49635cf0f63fd3c974ae5a177cf45bb1bb3ce
David García Díaz <davidgrcdiaz@gmail.com>
2024-03-21 17:05:27 +0100

    Add dotenv package and update webhook endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import * as express from 'express';
 import {createServer} from "node:http"
 import {Server} from "socket.io"
+import * as dotenv from 'dotenv'
+
+dotenv.config({
+  path: [__dirname + '/.env', __dirname + '/.env.local']
+})
+
+dotenv.config()
 
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
+const {WEBHOOK_SECRET} = process.env
+if (!WEBHOOK_SECRET) {
+  console.error('Missing webhook secret')
+  process.exit(1)
+}
 
+app.post('/webhook', (req, res) => {
+  const password = req.get('x-webhook-secret')
+  if (password !== WEBHOOK_SECRET) {
+    res.sendStatus(403)
+    return
+  }
+  io.emit('link', 'The links have been updated');
+  res.send('Links updated');
+})
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
+
+/*
 app.get('/', (req, res) => {
   res.send(`<doctype HTML>
   <html>
@@ -47,19 +74,4 @@ app.get('/', (req, res) => {
   </html>
 `);
 });
-app.post('/webhook', (req, res) => {
-  io.emit('chat message', 'A user has connected');
-  res.send('Links updated');
-  console.log(req)
-})
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    socket.broadcast.emit('chat message', 'A user has connected');
-  });
-});
-
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+*/
